3a95f6354f32b9862e7ba666bdbb1945d312a748
jrtanjr <jrtanjr@gmail.com>
2025-04-13 17:14:10 +0800

    Update bids.blade.php

--- resources/views/freelancer/bids.blade.php
+++ resources/views/freelancer/bids.blade.php
@@ -1,7 +1,7 @@
 <h1>{{ $user->name }}'s Bids</h1>
 @foreach($bids as $bid)
     <div>
-        <h2>{{ $bid->project->title }}</h2>
+        <h2>{{ $bid->project->title?? 'Project not found' }}</h2>
         <p>Amount: RM {{ $bid->bid_amount }}</p>
         <p>Status: {{ $bid->status }}</p>
         <a href="/bids/{{ $bid->id }}/edit">Edit</a>
